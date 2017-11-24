80420032624341ebc2555f2f738baf947c4557b3
damiclem <damiano.clementel@gmail.com>
2017-11-24 19:34:25 +0100

    Natural Language interfacing function and tag parsing functions have been implemented and tested

--- models/issue.js
+++ models/issue.js
@@ -1,6 +1,7 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var User = require('./user');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const User = require('./user.js');
+const Tag = require('./tag.js');
 
 var IssueSchema = new Schema({
   //document property: SchemaType
@@ -15,15 +16,60 @@ var IssueSchema = new Schema({
     parsed: String, //original, but lower-case without spaces
     relevance: Number //relevance is a double between 0 and 1
   }],
-  type: String //searching or found?
+  source: {
+    type: String,
+    enum: ['search', 'find']
+  } //searching or found?
 });
 
-//validating issue parameters
-IssueSchema.methods.checkInput = function() {}
+//using IBM watson to evaluate the sentence
+IssueSchema.methods.watson = function(next) {
+  var self = this;
 
-//adding tag elaboration API
-//using .methods given by schema
-IssueSchema.methods.generateTags = function() {}
+  //NLP used only here
+  var NaturalLanguageProcessor = require('watson-developer-cloud/natural-language-understanding/v1.js');
+
+  //setting up the connection to Watson API
+  var nlp = new NaturalLanguageProcessor({
+    'username': '080d9d59-c184-4cde-8361-8a9deb04e2a1',
+    'password': 'b0JW03HO1qYW',
+    'version_date': '2017-02-27'
+  });
+
+  //defining object params
+  var params = {
+    'text': self.description,
+    'features': {
+      'keywords': {
+        'sentiment': false,
+        'emotion': false,
+        'limit': 50
+      }
+    }
+  }
+
+  /*
+  //DEBUG
+  console.log('Parameters to be sent:')
+  console.log(params);
+  */
+
+  //executing analisys through Watson Natural Language Processor
+  nlp.analyze(params, next);
+}
+
+//add tags to this issue object
+IssueSchema.methods.addTags = function(keywords) {
+  this.tags = []; //empty array
+  keywords.forEach((keyword, i) => {
+    this.tags[i] = Tag.parseTag(keyword);
+  });
+}
+
+//sorting tags by parsed text
+IssueSchema.methods.sortTags = function() {
+  this.tags.sort(Tag.sortTagByParsed);
+}
 
 //exporting Issue object
 module.exports = mongoose.model('Issue', IssueSchema);
--- models/tag.js
+++ models/tag.js
@@ -1,10 +1,25 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var Issue = require('./issue');
+//parsing tag's text
+var parseTagText = function (textToParse) {
+	//deleting spaces
+	//to lower
+	return textToParse.replace(/\s/g,'').toLowerCase();
+}
+exports.parseTagText = parseTagText;
 
-var TagSchema = new Schema({
-	name: {type: String},
-	issues: [{type: mongoose.Schema.Types.ObjectId,  ref: 'Issue'}]
-});
+//returns the correct tag format
+exports.parseTag = function(tagToParse) {
+  return {
+    original: tagToParse.text, //setting original to retrieved keyword's text
+    parsed: parseTagText(tagToParse.text), //parsing original text
+    relevance: tagToParse.relevance //setting relevance to retrieved relevance
+  }
+}
 
-module.exports = mongoose.model('Tag', TagSchema);
+//sorting tag using 'parsed' attribute
+exports.sortTagByParsed = function(a, b) {
+	if (a.parsed > b.parsed)
+		return 1;
+	if (a.parsed < b.parsed)
+		return -1;
+	return 0;
+}
