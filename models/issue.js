53eda936477242bb2880cbfc6a8f19417a682dd1
Nicolò Pomini <nicolo.pomini@studenti.unitn.it>
2017-11-25 10:54:26 +0100

    preparing for merging with develop

--- models/issue.js
+++ models/issue.js
@@ -1,26 +1,27 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const User = require('./user.js');
-const Tag = require('./tag.js');
 
 var IssueSchema = new Schema({
   //document property: SchemaType
   time: {type: Date, default: null}, //when did I find it?
   inserted: {type: Date, default: Date.now()}, //when di I inserted this issue?
   room: {type: String, default: ''}, //where did i found it?
-  description: {type: String, default: ''}, //what
+  description: {type: String, minlength: 1}, //what
   author: {type: mongoose.Schema.Types.ObjectId,  ref: 'User'}, //who
   photo: {type: String, default: null},
   tags: [{
-    original: String, //original string retrieved from IBM Watson
-    parsed: String, //original, but lower-case without spaces
-    relevance: Number //relevance is a double between 0 and 1
+    original: {type: String, minlength: 1}, //original string retrieved from IBM Watson
+    parsed: {type: String, minlength: 1}, //original, but lower-case without spaces
+    relevance: {type: Number, min: 0, max: 1} //relevance is a double between 0 and 1
   }],
-  source: {
-    type: String,
-    enum: ['search', 'find']
-  } //searching or found?
+  type: {type: String, enum: ['searching','found']} //searching or found?
 });
+
+//function to sort tags
+IssueSchema.methods.sortTags = function(t1,t2) {
+	return t1.parsed.localeCompare(t2.parsed);
+}
 
 //using IBM watson to evaluate the sentence
 IssueSchema.methods.watson = function(next) {
