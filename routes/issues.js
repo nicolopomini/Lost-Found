322db17a88b0683f0f9bcb4b0c77cdb25c8ed4b8
Nicolò Pomini <nicolo.pomini@studenti.unitn.it>
2017-12-03 10:37:22 +0100

    GET API improved, and checked issue sort

--- routes/issues.js
+++ routes/issues.js
@@ -10,26 +10,6 @@ var Issue = require('../models/issue.js');
 
 var matching = require('../models/issue.matching');
 
-//SEARCHING FOR AN ITEM (USING DESCRIPTION)
-//search for matching issue by tag comparison
-router.get('/', function(req, res) {
-  //DEBUG
-  console.log('Search by DESCRIPTION');
-
-  //new issue from get parameters
-  var issue = new Issue();
-  issue.description = 'Prova';
-  issue.type = 'found';
-  issue.tags = [{original: 'AAAA', parsed: 'a ', relevance : 0.1}];
-  var error = issue.validateSync();
-  //TODO issue type = 'search'
-  console.log('New issue:');
-  console.log(issue);
-  console.log(error);
-
-  //DEBUG
-  res.send('OK');
-});
 
 //Matching the given issue with all the other issues
 router.get('/:issueid', function(req, res) {
@@ -50,64 +30,28 @@ router.get('/:issueid', function(req, res) {
       type: opposit_type,
       inserted: {$gt: limit}
     }, (err, issues) => {
+      issues.forEach((val, index) => {
+        console.log(val);
+        console.log(issues[index].tags);
+        //ORDINATI!!! GG
+      });
       if(err)
         hadleError(res, "Error during retriving issues into mongo");
       var rtr = {};
       rtr.error = "false";
-      rtr.issues = matching.match(issue,issues,1);
-      res.send(JSON.stringify(rtr));
+      //rtr.issues = matching.match(issue,issues,1);
+      res.json(rtr);
     });
   });
 });
 
-//FOUND ITEM
-//new issue from post parameters
-router.post('/', function(req, res) {
-  //DEBUG
-  console.log('Inserting FOUND');
-
-  //creating an Issue istance from POST parameters
-  var issue = new Issue(req.body);
-  //TODO issue type = 'found'
-  console.log('New issue:');
-  console.log(issue);
-
-  handleIssue(issue);
-});
 
 //handling errors in API functions
 function hadleError(res, error) {
   var toSend = {};
   toSend.error = error;
   toSend.issue = null;
-  res.send(JSON.stringify(toSend));
-}
-//handling issue search
-function handleIssue(issue) {
-  //issue attributes are not valid => no response
-  if(!issue.validAttr()) return;
-
-  //generating tags inside the issue class
-  issue.generateTags();
-
-  //saving the issue into the db
-  issue.save(function(err) {
-    //handling db errors
-    if (err) return handleError(err);
-    //saved!
-    console.log('Saved:');
-    console.log(issue);
-  }).then(function(){ //then() is used to assure that the new issue has been inserted
-    //searching for matching TagSchema
-    //search for issues with matching tags
-    Issue.find({}, function(err, res) {
-      //handling db errors
-      if(err) handleError(err);
-      //works!
-      console.log('Searching:');
-      console.log(res);
-    });
-  });
+  res.json(toSend);
 }
 
 module.exports = router;
