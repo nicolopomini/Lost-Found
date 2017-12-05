8b6a085a54b6d461d9ae52c89f0a9fab1b8a80b6
Nicolò Pomini <nicolo.pomini@studenti.unitn.it>
2017-12-05 09:52:44 +0100

    user setted up

--- models/user.js
+++ models/user.js
@@ -2,9 +2,14 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-	id: {type: String, minlength: 1},
+	id: {type: String, minlength: 1, unique: true},
 	name: {type: String, minlength: 1},
-	email: {type: String, minlength: 1}
+	email: {type: String, validate: {
+		validator: (v) => {
+			return /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/.test(v);
+		}
+	}, required: true
+	}
 });
 
 //returns if the user is authenticated or not
