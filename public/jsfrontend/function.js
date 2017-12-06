2417d05085c8c211a1349504e1bf2999b235c7e9
Iron512 <ivanmartini96@gmail.com>
2017-12-06 18:25:52 +0100

    prima versione frontend

--- public/jsfrontend/function.js
+++ public/jsfrontend/function.js
@@ -1,15 +1,3 @@
-var value = $(window).height();
-console.log(value);
-var height = "height:";
-height = height.concat(value,"px");
-
-document.getElementById("lost").setAttribute("style",height);
-document.getElementById("line").setAttribute("style",height);
-document.getElementById("found").setAttribute("style",height);
-
-var pos = 50 - $("#line").width()/2
-document.getElementById("and").setAttribute("style","left:".concat(-pos,"px"));
-
 var colorLost = "#2ECC40"; // green
 var colorFound = "#FFDC00"; // yellow
 var colorBack = "#FFFFFF"; // silver
@@ -20,15 +8,16 @@ var title = $("#h1lost").outerHeight(true)
 var usable = value - title;
 var viewport_form = usable * 80/100;
 
+//handle the click on "LOST" and "FOUND" labels
 $("#h1lost").click(function () {
 	document.getElementById("form-found").setAttribute("style","transition: all 0.5s;transition-delay:0s;");
 	document.getElementById("form-lost").setAttribute("style","height:".concat(viewport_form,"px;"));
 
 	document.getElementById("h1found").removeAttribute("style");
-	document.getElementById("h1lost").setAttribute("style","color:".concat(colorBack));
+	document.getElementById("h1lost").setAttribute("style","color:".concat(colorLost));
 
 	document.getElementById("lost").removeAttribute("style");
-	document.getElementById("lost").setAttribute("style","background-color:".concat(colorLost,";width:65.8%"));
+	document.getElementById("lost").setAttribute("style","background-color:".concat(colorBack,";width:65.8%"));
 
 	document.getElementById("found").removeAttribute("style");	
 	document.getElementById("found").setAttribute("style","width:33.8%");
@@ -39,10 +28,10 @@ $("#h1found").click(function () {
 	document.getElementById("form-found").setAttribute("style","height:".concat(viewport_form,"px;"));
 
 	document.getElementById("h1lost").removeAttribute("style");
-	document.getElementById("h1found").setAttribute("style","color:".concat(colorBack));
+	document.getElementById("h1found").setAttribute("style","color:".concat(colorFound));
 
 	document.getElementById("found").removeAttribute("style");
-	document.getElementById("found").setAttribute("style","background-color:".concat(colorFound,";width:65.8%"));
+	document.getElementById("found").setAttribute("style","background-color:".concat(colorBack,";width:65.8%"));
 
 	document.getElementById("lost").removeAttribute("style");
 	document.getElementById("lost").setAttribute("style","width:33.8%");
