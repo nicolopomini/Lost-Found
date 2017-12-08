4ea751ab2fd699e72ab1d8970c2d50bb2c43e626
damiclem <damiano.clementel@gmail.com>
2017-12-08 12:56:40 +0100

    Fixed login bug

--- routes/auth.js
+++ routes/auth.js
@@ -64,7 +64,7 @@ passport.use(new GoogleStrategy({
         });
 
         //checking if user params are validate
-        if(user.validateSync()) {
+        if(typeof user.validateSync() != 'undefined') {
           done(new Error('Parametri utente non validi!'), false);
         }
         else {
