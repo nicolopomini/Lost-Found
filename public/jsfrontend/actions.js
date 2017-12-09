7b6799e9a916f3804684cdd4dd6b0cb67cbdefb7
Nicolò Pomini <nicolo.pomini@studenti.unitn.it>
2017-12-09 11:59:24 +0100

    Desktop frontend almost done, just need to display results

--- public/jsfrontend/actions.js
+++ public/jsfrontend/actions.js
@@ -1,95 +1,7 @@
-/*var user = null;
-//id, displayName, email
-document.getElementById('loginbtn').setAttribute('href', 'auth/login');;
-var sessionrequest = new XMLHttpRequest();
-sessionrequest.onreadystatechange = function() {
-	if (this.readyState == 4 && this.status == 200) {
-		var content = JSON.parse(this.responseText);
-		var logged = content != 'false'; //|| altra condizione
-		if(logged)
-			user = content;
-		else
-			user = null;
-		document.getElementById('loginbtn').setAttribute('href', logged ? 'auth/logout' : 'auth/login');
-
-	}
-};
-sessionrequest.open('POST','session',false);
-sessionrequest.send();
-
-function formHandler(type) {
-	console.log(type);
-	if(user == null)
-	{
-		alert("You must be signed in.");
-		return;
-	}
-	var description = null;
-	var time = null;
-	var room = null;
-	if(type == 'lost') {
-		description = document.getElementById('lost-description').value;
-		time = document.getElementById('lost-time').value;
-		room = document.getElementById('lost-room').value;
-	}
-	else if(type == 'found') {
-		description = document.getElementById('found-description').value;
-		time = document.getElementById('found-time').value;
-		room = document.getElementById('found-room').value;
-	} else {
-		alert("Error");
-		return;
-	}
-	if(description == '' || description == null) {
-		alert("Error, description needed");
-		return;
-	}
-	var toSend = 'token=' + user._id + '&description=' + description;
-	if(time != '' && time != null)
-		toSend = toSend.concat('&time=' + time);
-	if(room != '' && room != null)
-		toSend = toSend.concat('&room=' + room);
-	var request = new XMLHttpRequest();
-	request.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			//reset forms
-			document.getElementById('lost-form').reset();
-			document.getElementById('found-form').reset();
-			var response = JSON.parse(this.responseText);
-			if(response.error == false) {	//nessun errore
-				var matchingurl = 'issues/' + response.issue + '?token=' + user._id;
-				var matchrequest = new XMLHttpRequest();
-				matchrequest.onreadystatechange = function() {
-					if (this.readyState == 4 && this.status == 200) {
-						var secondresponse = JSON.parse(this.responseText);
-						console.log(secondresponse);
-						if(secondresponse.error == 'false') { //nessun errore
-							console.log(secondresponse.issues);	//array di issues
-						}
-						else
-							alert(secondresponse.error);
-					}
-				}
-				matchrequest.open('GET', matchingurl, true);
-				matchrequest.send();
-			} else
-				alert(response.error);
-		}
-	};
-	var url = 'issues/';
-	if(type == 'lost')
-		url = url.concat('search');
-	else
-		url = url.concat('found');
-	request.open('POST', url, true);
-	request.setRequestHeader("Content-Type", "application/json");
-	request.send(JSON.stringify(toSend));
-}*/
-
 function emptyForm($form) {
 	$form.find('input').each(function(i){
 		console.log($(this));
-		//$(this).val('').change();
+		$(this).val('')
 	});
 }
 
@@ -99,6 +11,14 @@ function formHandler(ref, api) {
 	var $this = $(ref);
 	//retrieving form params
 	var params = $this.closest('form').serializeArray();
+	if(params[0].value == '') {	//not logged
+		alert("Please sign in with an university account");
+		return;
+	}
+	if(params[3].value == '') {	//empty description
+		alert("Your issue cannot be empty");
+		return;
+	}
 	$.post('/issues/' + api, params)
 		.done(function(res){
 			if (!res.error) {
@@ -110,8 +30,14 @@ function formHandler(ref, api) {
 							//refresh the form
 							emptyForm($this);
 							//TODO: fill the result table
+							console.log(res.issues);
+						}
+						else {	//error during issue match
+							alert(res.error);
 						}
 					});
+			} else {	//error during issue insertion
+				alert(res.error);
 			}
 		});
 }
