a8d6b70a060204adc171908e66edbb41c3a853e9
Iron512 <ivanmartini96@gmail.com>
2017-12-10 21:01:24 +0100

    minor fixes, merged locally develop and frontend

--- public/jsfrontend/function.js
+++ public/jsfrontend/function.js
@@ -8,31 +8,54 @@ var title = $("#h1lost").outerHeight(true)
 var usable = value - title;
 var viewport_form = usable * 80/100;
 
-//handle the click on "LOST" and "FOUND" labels
-$("#h1lost").click(function () {
-	document.getElementById("form-found").setAttribute("style","transition: all 0.5s;transition-delay:0s;");
-	document.getElementById("form-lost").setAttribute("style","height:".concat(viewport_form,"px;"));
+if (mobile) {
+	console.log("mobile")
+	//import CSS
+	var head  = document.getElementsByTagName('head')[0];
+	var link  = document.createElement('link');
+	link.rel  = 'stylesheet';
+	link.href = '/stylesheets/mobile.css';
+	head.appendChild(link);
 
-	document.getElementById("h1found").removeAttribute("style");
-	document.getElementById("h1lost").setAttribute("style","color:".concat(colorLost));
+	//handle the click on "LOST" and "FOUND" labels
+	$("#h1lost").click(function () {
+		
+	})
 
-	document.getElementById("lost").removeAttribute("style");
-	document.getElementById("lost").setAttribute("style","background-color:".concat(colorBack,";width:65.8%"));
+	$("#h1found").click(function () {
 
-	document.getElementById("found").removeAttribute("style");	
-	document.getElementById("found").setAttribute("style","width:33.8%");
-})
+	})
 
-$("#h1found").click(function () {
-	document.getElementById("form-lost").setAttribute("style","transition: all 0.5s;transition-delay:0s;");
-	document.getElementById("form-found").setAttribute("style","height:".concat(viewport_form,"px;"));
+} else {
+	console.log("desktop")
 
-	document.getElementById("h1lost").removeAttribute("style");
-	document.getElementById("h1found").setAttribute("style","color:".concat(colorFound));
+	//handle the click on "LOST" and "FOUND" labels
+	$("#h1lost").click(function () {
+		document.getElementById("form-found").setAttribute("style","transition: all 0.5s;transition-delay:0s;");
+		document.getElementById("form-lost").setAttribute("style","height:".concat(viewport_form,"px;"));
 
-	document.getElementById("found").removeAttribute("style");
-	document.getElementById("found").setAttribute("style","background-color:".concat(colorBack,";width:65.8%"));
+		document.getElementById("h1found").removeAttribute("style");
+		document.getElementById("h1lost").setAttribute("style","color:".concat(colorLost));
 
-	document.getElementById("lost").removeAttribute("style");
-	document.getElementById("lost").setAttribute("style","width:33.8%");
-})
+		document.getElementById("lost").removeAttribute("style");
+		document.getElementById("lost").setAttribute("style","background-color:".concat(colorBack,";width:65.8%"));
+
+		document.getElementById("found").removeAttribute("style");	
+		document.getElementById("found").setAttribute("style","width:33.8%");
+	})
+
+	$("#h1found").click(function () {
+		document.getElementById("form-lost").setAttribute("style","transition: all 0.5s;transition-delay:0s;");
+		document.getElementById("form-found").setAttribute("style","height:".concat(viewport_form,"px;"));
+
+		document.getElementById("h1lost").removeAttribute("style");
+		document.getElementById("h1found").setAttribute("style","color:".concat(colorFound));
+
+		document.getElementById("found").removeAttribute("style");
+		document.getElementById("found").setAttribute("style","background-color:".concat(colorBack,";width:65.8%"));
+
+		document.getElementById("lost").removeAttribute("style");
+		document.getElementById("lost").setAttribute("style","width:33.8%");
+	})
+}
+
